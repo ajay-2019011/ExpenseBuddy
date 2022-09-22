3a66a47d2125c79e1f15b3e8dd49b3400d09de3c
ajay-2019011 <72451943+ajay-2019011@users.noreply.github.com>
2022-09-23 00:45:21 +0530

    Frebase configurations done for the project

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -1,6 +1,6 @@
 import firebase from '@firebase/app';
 import "@firebase/auth";
-
+//Firebase config set
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
